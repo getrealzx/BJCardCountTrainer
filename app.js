2a380f2452c9a59f7a6dc5ffbcfc144ca416a59f
foorkan <furqanshabbir2010@gmail.com>
2020-03-18 15:03:25 -0500

    blackjack

--- app.js
+++ app.js
@@ -4,9 +4,9 @@ const app = express();
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
-app.use(require('./routes/'));
-app.use(require('./routes/blogs.js'));
-app.use(require('./routes/editblogs.js'));
+app.use(require('./views/login.ejs/'));
+// app.use(require('routes/login.js'));
+// app.use(require('./routes/editblogs.js'));
 
 app.listen(3000, () => {
   console.log('Listening on 3000');
